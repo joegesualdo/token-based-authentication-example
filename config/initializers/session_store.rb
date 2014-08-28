24c54c581e080e282bfbf25301cb3cb4a5c03b40
Joe Gesualdo <joegesualdo@gmail.com>
2014-08-27 20:02:33 -0400

    Change project name back to original name

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_token_based_authentication_example_session'
+Rails.application.config.session_store :cookie_store, key: '_tasks_session'
